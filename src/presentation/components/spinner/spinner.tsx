53197393a56562ae8adc154780fc6b57084200a2
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-01-18 23:46:32 -0300

    feat: add login view

---
+++ src/presentation/components/spinner/spinner.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Styles from './spinner-styles.scss'
+
+type Props = React.HTMLAttributes<HTMLDivElement>
+
+const Spinner: React.FC<Props> = (props: Props) => (
+  <div {...props} className={[Styles.spinner, props.className].join(' ')}>
+    <div/><div/><div/><div/>
+  </div>
+)
+
+export default Spinner
