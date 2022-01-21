e1d1993e7b82954a945fd4d7105403dedd371744
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-01-21 01:55:20 -0300

    test: ensure Login calls validation with correct password

--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -50,4 +50,12 @@ describe('Login component', () => {
       email: 'any_email'
     })
   })
+  it('should call validation with correct email', () => {
+    const { sut, validationSpy } = makeSut()
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: 'any_password' } })
+    expect(validationSpy.input).toEqual({
+      password: 'any_password'
+    })
+  })
 })
