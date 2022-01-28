7ffaf25301cfc9b00218ecdea7febc09c42d34e1
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-01-28 03:55:14 -0300

    test: ensure EmailValidation returns falsy if email is valid

--- src/validations/validators/email/email-validation.spec.ts
+++ src/validations/validators/email/email-validation.spec.ts
@@ -1,10 +1,16 @@
 import { InvalidFieldError } from '@/validations/errors/invalid-field-error'
 import { EmailValidation } from './email-validation'
+import faker from 'faker'
 
 describe('EmailValidation', () => {
   it('should return error if email is invalid', () => {
-    const sut = new EmailValidation('email')
-    const error = sut.validate('')
+    const sut = new EmailValidation(faker.random.words())
+    const error = sut.validate(faker.random.words())
     expect(error).toEqual(new InvalidFieldError())
+  })
+  it('should return falsy if email is valid', () => {
+    const sut = new EmailValidation(faker.random.words())
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
