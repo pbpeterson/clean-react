32ddf1601308758b526f58f2f613067b3d148ddf
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-04-07 04:40:49 -0300

    test: ensure Textfield component removes readOnly on focus

--- src/presentation/components/Textfield/input.spec.tsx
+++ src/presentation/components/Textfield/input.spec.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import { fireEvent, render, RenderResult } from '@testing-library/react'
 import TextField from './textfield'
 import Context from '@/presentation/context/formContext'
+import faker from 'faker'
 
 const makeSut = (name: string): RenderResult => {
   const result = render(
@@ -16,10 +17,19 @@ const makeSut = (name: string): RenderResult => {
 
 describe('TextField', () => {
   it('should begin with readonly', () => {
-    const inputName = 'field'
+    const inputName = faker.database.column()
     const sut = makeSut(inputName)
     const input = sut.getByTestId(inputName) as HTMLInputElement
 
     expect(input.readOnly).toBe(true)
+  })
+  it('should remove readonly on focus', () => {
+    const inputName = faker.database.column()
+    const sut = makeSut(inputName)
+    const input = sut.getByTestId(inputName) as HTMLInputElement
+
+    fireEvent.focus(input)
+
+    expect(input.readOnly).toBe(false)
   })
 })
